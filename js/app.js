efe97c3ae420f73539ff4173565fe653ca9bcb5b
fuzzbdn <magnus.moreskog@gmail.com>
2026-05-21 09:25:32 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,19 +7,18 @@ import { initGranskaPage } from './pages/granska.js';
 import { initAktivaPage } from './pages/aktiva.js';
 import { initSavedPage } from './pages/debug.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('loginForm');
-    if (loginForm) {
-        initLoginPage(loginForm);
-    } else {
-        checkLoginStatus();
-    }
+// Vi kör koden direkt - type="module" väntar automatiskt in HTML-laddningen
+const loginForm = document.getElementById('loginForm');
+if (loginForm) {
+    initLoginPage(loginForm);
+} else {
+    checkLoginStatus();
+}
 
-    if (document.getElementById('planera-page')) initPlaneraPage();
-    if (document.getElementById('projektera-page')) initProjekteraPage();
-    if (document.getElementById('radera-page')) initRaderaPage();
-    if (document.getElementById('users-page')) initUsersPage();
-    if (document.getElementById('granska-page')) initGranskaPage();
-    if (document.getElementById('aktiva-page')) initAktivaPage();
-    if (document.getElementById('saved-page')) initSavedPage();
-});
+if (document.getElementById('planera-page')) initPlaneraPage();
+if (document.getElementById('projektera-page')) initProjekteraPage();
+if (document.getElementById('radera-page')) initRaderaPage();
+if (document.getElementById('users-page')) initUsersPage();
+if (document.getElementById('granska-page')) initGranskaPage();
+if (document.getElementById('aktiva-page')) initAktivaPage();
+if (document.getElementById('saved-page')) initSavedPage();
